5712c2b826a34f4f67ab8d77f6f17101cb36b6c7
Woo Sung Kang <geniusraban@gmail.com>
2022-07-06 00:33:37 +0900

    Routers for register and log-in

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
-const session = require("express-session");
 const bodyparser = require("body-parser");
 const cookies = require("cookie-parser");
-const {v4 : uuidv4} = require("uuid");
 const router = require('./router.js');
+const userRoutes = require('./server/routes/users');
+const authRoutes = require('./server/routes/auth');
+const cors = require("cors");
 const app = express();
 
 const connectDB = require('./server/database/connection');
@@ -13,27 +14,21 @@ const connectDB = require('./server/database/connection');
 const PORT = process.env.PORT || 3000;
 
 dotenv.config({path:'config.env'})
-
+//connect with mongo
 connectDB();
-
+//middleware
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended : true}))
+app.use(express.json());
+app.use(cors());
 
-/* //load static assests
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
-
-app.use(session({
-    secret : uuidv4(), //this method will make this session completely secret and unique
-    resave : false,
-    saveUninitialized : true
-})); */
-
-app.use('/route', router);
+//routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 //home route
 app.set('view engine', 'ejs');
-
+ 
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}/`)
