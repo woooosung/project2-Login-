0ff8394f39d4eb64c852bd3a9dc527674f4f3288
Woo Sung Kang <geniusraban@gmail.com>
2022-07-05 21:54:33 +0900

    mongo db connected

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv");
 const session = require("express-session");
 const bodyparser = require("body-parser");
 const cookies = require("cookie-parser");
 const {v4 : uuidv4} = require("uuid");
 const router = require('./router.js');
-
 const app = express();
+
+const connectDB = require('./server/database/connection');
 
 const PORT = process.env.PORT || 3000;
 
-const connectDB = require('./server/database/connection');
+dotenv.config({path:'config.env'})
 
 connectDB();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended : true}))
- 
-//load static assests
+
+/* //load static assests
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 
@@ -25,16 +27,13 @@ app.use(session({
     secret : uuidv4(), //this method will make this session completely secret and unique
     resave : false,
     saveUninitialized : true
-}));
+})); */
 
 app.use('/route', router);
 
 //home route
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('base', {title:"Login System"})
-}); 
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}/`)
--- server/database/connection.js
+++ server/database/connection.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async() => {
+    const connectionParams = {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+    };
     try{
         //mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URI, {
-        })
-
+        const con = await mongoose.connect(process.env.MONGO_URI, connectionParams)
         console.log(`MongoDB connected : ${con.connection.host}`);
     }catch(err){
         console.log(err);
         process.exit(1);
     }
 }
-
-module.exports = connectDB
+ 
+module.exports = connectDB 
